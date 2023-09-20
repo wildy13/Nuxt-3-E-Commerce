275e1b1610912c979a8cf460cffb63e90a4854ac
wildy13 <wildy.simanjuntak13@gmail.com>
2023-09-20 07:23:41 +0700

    change color actived

--- frontend/components/general/Navbar.vue
+++ frontend/components/general/Navbar.vue
@@ -3,22 +3,22 @@
     <div class="flex justify-between items-center mx-4">
       <div>This is Logo</div>
       <div class="flex items-center gap-4">
-        <nuxt-link to="/" exact active-class="text-slate-600">
-          <div class="hover:text-slate-600 cursor-pointer">Home</div>
+        <nuxt-link to="/" exact active-class="text-slate-800">
+          <div class="hover:text-slate-800 cursor-pointer">Home</div>
         </nuxt-link>
-        <nuxt-link to="/product" exact active-class="text-slate-600">
-          <div class="hover:text-slate-600 cursor-pointer">Product</div>
+        <nuxt-link to="/product" exact active-class="text-slate-800">
+          <div class="hover:text-slate-800 cursor-pointer">Product</div>
         </nuxt-link>
-        <nuxt-link to="/contact" exact active-class="text-slate-600">
-          <div class="hover:text-slate-600 cursor-pointer">Contact</div>
+        <nuxt-link to="/contact" exact active-class="text-slate-800">
+          <div class="hover:text-slate-800 cursor-pointer">Contact</div>
         </nuxt-link>
-        <nuxt-link to="/auth/login" exact active-class="text-slate-600">
+        <nuxt-link to="/auth/login" exact active-class="text-slate-800">
           <UButton
             icon="i-heroicons-arrow-left-on-rectangle"
             size="xl"
             color="white"
             variant="link"
-            class="hover:text-slate-600"
+            class="hover:text-slate-800"
           />
         </nuxt-link>
       </div>
