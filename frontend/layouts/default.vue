bb1bbd82f6a1242edff438d0229526660ac18234
wildy13 <wildy.simanjuntak13@gmail.com>
2023-09-25 16:42:46 +0700

    update

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="h-screen">
         <Navbar />
-        <div class="container mx-auto pt-24">
+        <div class="container mx-auto pt-24 pb-14">
             <slot />
         </div>
-        <Footer />
+        <Footer class="fixed bottom-0 w-full z-10  " />
     </div>
 </template>
