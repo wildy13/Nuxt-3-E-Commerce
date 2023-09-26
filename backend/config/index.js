87811aeed577e3c5a110271de5124385c3c11c19
wildy13 <wildy.simanjuntak13@gmail.com>
2023-09-26 12:32:48 +0700

    update

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,21 +1,25 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Sequelize = require('sequelize');
-const mysql2 = require('mysql2')
+const Sequelize = require("sequelize");
+const mysql2 = require("mysql2");
 
-const sequelize = new Sequelize(
-    process.env.MYSQL_CONNECT_URI,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
-        dialectModule: mysql2,
-    }
-)
+const sequelize = new Sequelize(process.env.MYSQL_CONNECT_URI, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DIALECT,
+  dialectModule: mysql2,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+});
 
 const config = {
-    host : process.env.BACKEND_HOST,
-    port : process.env.BACKEND_PORT,
-    secret: { session: process.env.SESSION_KEY },
-}
+  host: process.env.BACKEND_HOST,
+  port: process.env.BACKEND_PORT,
+  secret: { session: process.env.SESSION_KEY },
+};
 
-module.exports = { sequelize, config }
+module.exports = { sequelize, config };
