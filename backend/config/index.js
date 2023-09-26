72d6f6fbc764979d3069b5614bcb178d5160024a
wildy13 <wildy.simanjuntak13@gmail.com>
2023-09-26 11:49:20 +0700

    update

--- backend/config/index.js
+++ backend/config/index.js
@@ -4,24 +4,7 @@ const Sequelize = require('sequelize');
 const mysql2 = require('mysql2')
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD,
-    {
-        host : process.env.BACKEND_HOST,
-        dialect : process.env.DB_DIALECT,
-        dialectModule: mysql2,
-        dialectOptions: {
-            dateStrings: true,
-            typeCast: true,
-        },
-        pool: {
-            max: 10,
-            min: 0,
-            idle: 10000,
-        },
-          
-    }
+    process.env.MYSQL_CONNECT_URI,
 )
 
 const config = {
