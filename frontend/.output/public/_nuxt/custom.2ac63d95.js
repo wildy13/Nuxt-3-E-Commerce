106ba4503a6ba44534dcae8ae23e4f11c494c886
wildy13 <wildy.simanjuntak13@gmail.com>
2023-09-26 13:27:25 +0700

    vercel

---
+++ frontend/.output/public/_nuxt/custom.2ac63d95.js
@@ -0,0 +1,1 @@
+import{_ as o,c as r,r as t,o as c}from"./entry.2c50f5b2.js";const s={};function n(e,a){return c(),r("div",null,[t(e.$slots,"default")])}const _=o(s,[["render",n]]);export{_ as default};
