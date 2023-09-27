98f04b5bab6897e0d8c062f415b85f1e59fceef8
wildy13 <wildy.simanjuntak13@gmail.com>
2023-09-27 10:27:31 +0700

    update

--- backend/router.js
+++ backend/router.js
@@ -4,6 +4,7 @@ const product = require('./api/product')
 const category = require('./api/category')
 const carts = require('./api/carts')
 const cart_items = require('./api/cart_items')
+const role = require('./api/role')
 
 const route = async (fastify) => {
     fastify.register(user, { prefix: 'api/user'})
@@ -12,6 +13,7 @@ const route = async (fastify) => {
     fastify.register(category, { prefix: 'api/category'})
     fastify.register(carts, { prefix: 'api/carts'})
     fastify.register(cart_items, { prefix: 'api/cart_items'})
+    fastify.register(role, { prefix: 'api/role'})
 };
 
 module.exports = route
