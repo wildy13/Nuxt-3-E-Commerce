2724459baf220380ef1958b78e42b56cca7aa323
wildy13 <wildy.simanjuntak13@gmail.com>
2023-09-27 16:50:03 +0700

    update vercel

---
+++ frontend/.output/public/_nuxt/index.0da13549.js
@@ -0,0 +1,1 @@
+import{_ as o,o as e,c as t,a as s,g as r}from"./entry.4678399a.js";const a={},i=s("div",null,"This is Home",-1),m=[i];function d(_,c){return e(),t("div",null,m)}const l=o(a,[["render",d]]),f={__name:"index",setup(_){return(c,p)=>{const n=l;return e(),r(n)}}};export{f as default};
